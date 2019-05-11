a4ce961e139b79d961a17d035bfc6d42ffe2d9ee
wanggangfeng <wanggangfeng@forchange.tech>
2019-05-11 18:08:57 +0800

    增加goroutine

--- channel/goroutine/goroutine.go
+++ channel/goroutine/goroutine.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	"time"
 )
+
 //不能在函数再定义其他非匿名函数 --所以函数传参的必要
-func worker(id int,c chan int) {
+func worker(id int, c chan int) {
 	for {
 		n, ok := <-c
 		if !ok {
@@ -20,21 +21,21 @@ func worker(id int,c chan int) {
 	//}
 
 }
-func main(){
-	  //注意channel是goroutine之间的通道，也就是起码至少两个goroutine
-	 // A-channel-B
-	 //这就是为什么需要在一个goroutine去读或者写
-	 //以下这种写法会造成死锁，因为只有一个goroutine对象没有另外一个怎么建立通道
-	 //c := make(chan int)
-	 //fmt.Println( <- c)
-	 //有两个goroutine进行通信 --但是我变化下我先把读的函数写在前面，比如：
+func main() {
+	//注意channel是goroutine之间的通道，也就是起码至少两个goroutine
+	// A-channel-B
+	//这就是为什么需要在一个goroutine去读或者写
+	//以下这种写法会造成死锁，因为只有一个goroutine对象没有另外一个怎么建立通道
+	//c := make(chan int)
+	//fmt.Println( <- c)
+	//有两个goroutine进行通信 --但是我变化下我先把读的函数写在前面，比如：
 	//c := make(chan int)
 	//A
 	// go func(){
 	//	 fmt.Println("读取")
 	// 	 fmt.Println(<-c)
 	// }()
-	 //fmt.Println("写入")
+	//fmt.Println("写入")
 	//B
 	// c<-11
 	//C
@@ -44,7 +45,7 @@ func main(){
 	//}()
 	//fmt.Println("写入")
 	// c<-22
-	 /*
+	/*
 	  读取
 	  写入
 	  11
@@ -55,23 +56,23 @@ func main(){
 
 	 结果是以上的 我们不能按照代码同步的逻辑去分析，而是按照通信的，而且goroutine是异步的
 	  fmt.Println(<-c)是读取，发现B还没有发信息，我阻塞了--等它发信息过来我再执行
-	 */
-	 //升级 我们写个死循环不断接收数据
+	*/
+	//升级 我们写个死循环不断接收数据
 
-    // func we(){
-    // 	println("11")
+	// func we(){
+	// 	println("11")
 	//}
 
-	 c := make(chan int)
-	 go worker(0,c)
+	c := make(chan int)
+	go worker(0, c)
 
-     c <- 11
-     c <- 22
-    // c <- 'a' //a--ascii 97?
-     //c <- 'a的' 会报错
-     //增加close
-     close(c)
-     time.Sleep(20)
+	c <- 11
+	c <- 22
+	// c <- 'a' //a--ascii 97?
+	//c <- 'a的' 会报错
+	//增加close
+	close(c)
+	time.Sleep(20)
 
 }
 
