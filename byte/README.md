38220db4d0bad33d222b0fe9fcbc292d7db8a12f
wanggangfeng <wanggangfeng@forchange.tech>
2019-05-18 23:19:14 +0800

    新增string,rune,byte,slice笔记

---
+++ byte/README.md
@@ -0,0 +1,107 @@
+## 浅谈slice、byte、string、rune
+
+
+### slice(引用类型)
+> 在很多应用场景中，数组并不能满足我们的需求。在初始定义数组时，我们并不知道需要多大的数组，因此我们就需要“动态数组”。在Go里面这种数据结构叫slice
+
+`slice`并不是真正意义上的动态数组，而是一个引用类型。slice总是指向一个底层`array`，只是不需要长度。slice的声明也可以像array一样
+
+声明：
+```go
+//声明一个保存 int 的 slice
+var iSlice []int
+
+//声明一个长度为 10 的 int 数组
+var iArray [10]int
+
+//还有一种声明的方法是使用 make() 函数，如下
+slice1 := make([]int, 5, 10)
+//用 make() 函数创建的时候有三个参数，make(type, len[, cap]) ，依次是类型、长度、容量。
+```
+ slice底层代码
+```go
+type IntSlice struct {
+    ptr      *int
+    len, cap int
+
+```
+**注意`slice`和数组在声明时的区别：声明数组时，方括号内写明了数组的长度或使用`...`自动计算长度，而声明`slice`时，方括号内没有任何字符。**
+
+slice有一些简便的操作
+
+ - `slice`的默认开始位置是0，`ar[:n]`等价于`ar[0:n]`
+ - `slice`的第二个序列默认是数组的长度，`ar[n:]`等价于`ar[n:len(ar)]`
+ - 如果从一个数组里面直接获取`slice`，可以这样`ar[:]`，因为默认第一个序列是0，第二个是数组的长度，即等价于`ar[0:len(ar)]`
+ 
+ 
+ ### string
+ >，Go中的字符串都是采用UTF-8字符集编码。字符串是用一对双引号（""）或反引号（` `）括起来定义，它的类型是string
+ 
+ **注意go语言的string类型与其他语言的string类型可能不一样，比如看以下代码:**
+ 
+ ```go
+     s3 :="a我爱中国"
+	for _,ss:=range s3{
+		/*
+		 97
+		25105
+		29233
+		20013
+		22269
+		*/
+		fmt.Println(ss)
+	}
+```
+**很神奇的一件事就是string能被像数组一样循环，我们来看看string的底层源码**
+```go
+	 type stringStruct struct {
+	    str unsafe.Pointer
+	    len int
+	}
+//实际上string是个结构体指针指向[]byte，也就是说string底层是byte数组，所以可以被循环，也就是可以通过len方法知道具体长度（中英文长度可能不一样，稍后再说）
+
+```
+-在Go中字符串是不可变的,例如下面的代码编译时会报错：cannot assign to s[0]
+```go
+var s string = "hello"
+s[0] = 'c
+```
+但如果真的想要修改怎么办呢？下面的代码可以实现：
+```go
+//其实这样的修改就是拷贝
+s := "hello"
+c := []byte(s)  // 将字符串 s 转换为 []byte 类型
+//"c"会报错 因为这是字符串而不是字符字符用单引号表示''
+c[0] = 'c' //这样的复制很奇怪 因为byte是uint8也就是数值 隐式转化？我的猜测
+s2 := string(c)  // 再转换回 string 类型
+fmt.Printf("%s\n", s2)
+```
+- string的结构体指针指向的是byte数组
+
+### byte与rune
+来看个有趣的现象：
+```go
+ first :="first"
+  fmt.Println([]rune(first))  //[102 105 114 115 116]
+  fmt.Println([]byte(first))  //[102 105 114 115 116]
+ china :="你好，中国"
+   fmt.Println([]rune(china)) //[20320 22909 65292 20013 22269]
+   fmt.Println([]byte(china)) //[228 189 160 229 165 189 239 188 140 228 184 173 229 155 189]
+```
+>byte类型对应着uint8，说到byte就不得不说中文情况下，字符是占多少个字节，因为前面说了字符串底层是指向byte数组的，首先go的编码形式是utf-8编码
+>而rune是int32的别名，代表字符的Unicode编码，采用4个字节存储，将string转成rune就意味着任何一个字符都用4个字节来存储其unicode值，这样每次遍历的时候返回的就是unicode值，而不再是字节了，这样就可以解决乱码问题了
+- UTF-8编码：一个英文字符等于**一个字节**，一个中文（含繁体）等于**三个字节**。中文标点占三个字节，英文标点占一个字节
+- Unicode：计算机是二进制的，字符最终也是转换成二进制保存起来的。字符集就是定义字符对应的数值。 Unicode是一个字符集，为每个字符规定一个用来表示该字符的数字，但是并没有规定该数字的二进制保存方式，utf8规定了对于unicode值的二进制保存方式
+ **大白话就是一组数字对应一个字符**
+- go用rune来表示unicode码点，也就是rune类型字符都是占**四个字节**不管中英文
+
+
+### 为什么slice打印出来的是字符而不是byte数组
+
+```go
+  s3 :="a我爱中国"
+	fmt.Println(s3[:]) //a我爱中国
+	fmt.Println(s3[:2])//a�
+	fmt.Println(s3[:4])//a我 0123   以字节来算的
+```
+//go内部这样转化为string([]byte(s))？
---
+++ regexp/main.go
@@ -0,0 +1,162 @@
+package  main
+
+import "regexp"
+
+//全局匹配or局部匹配
+/*
+正则表达式都是针对于字符串（还有数字这类的）所以string方法会很多
+go正则的ALL指的是全局搜索 方法没有all不是全局搜索 all全局搜索如果需要int参数
+-1代表返回全部 0表示一个
+func (re *Regexp) FindAllStringSubmatch(s string, n int) [][]string {
+	//代表如果小于0返回全部匹配到的字段
+	if n < 0 {
+		n = len(s) + 1
+	}
+	var result [][]string
+	re.allMatches(s, nil, n, func(match []int) {
+		if result == nil {
+			result = make([][]string, 0, startSize)
+		}
+		slice := make([]string, len(match)/2)
+		for j := range slice {
+			if match[2*j] >= 0 {
+				slice[j] = s[match[2*j]:match[2*j+1]]
+			}
+		}
+		result = append(result, slice)
+	})
+	return result
+}
+*/
+//正则字符含义
+/*
+	代码	说明
+	.	匹配除换行符以外的任意字符
+	\w	匹配字母或数字或下划线或汉字
+	\s	匹配任意的空白符
+	\d	匹配数字
+	\b	匹配单词的开始或结束
+	^	匹配字符串的开始
+	$	匹配字符串的结束
+*/
+//正则方法
+/*
+regexp是公共方法 r是结构体
+regexp.MatchString
+regexp.Compile
+r.MatchString
+r.FindString
+r.FindStringIndex
+r.FindStringSubmatch 这个方法返回全局匹配的字符串和局部匹配的字符，比如
+FindStringSubmatchIndex 和上面的方法一样，不同的是返回全局匹配和局部匹配的 起始索引和结束索引
+r.FindAllString 个方法返回所有正则匹配的字符，不仅仅是第一个（全局搜索/匹配）
+r.FindAllStringSubmatchIndex 这个方法返回所有全局匹配和局部匹配的字符串起始索引和结束索引
+r.FindAllString  为这个方法提供一个正整数参数来限制匹配数量
+regexp.MustCompile  当使用正则表达式来创建常量的时候，你可以使用`MustCompile` 因为`Compile`返回两个值
+r.ReplaceAllString
+r.ReplaceAllFunc
+*/
+
+/*
+[里面的表达式是||或者不是&&]
+[]//里面放的值就是实际的值 不是正则的匹配符号那样的 比如你a-z表示a-z的范围，放.就表示点 而不是表示任意字符
+[a-z.]==>这样理解为 a-z||.(小数点)
+compile生成正则表达式
+*/
+func main(){
+
+	//flysnowRegexp := regexp.MustCompile(`^http://www.flysnow.org/([\d]{4})/([\d]{2})/([\d]{2})/([\w-]+).html$`)
+	//params := flysnowRegexp.FindStringSubmatch("http://www.flysnow.org/2018/01/20/golang-goquery-examples-selector.html")
+    //fmt.Println(params)
+	//for _,param :=range params {
+	//	fmt.Println(param)
+	//}
+	//FindStringSubmatch方法是提取出匹配的字符串，然后通过[]string返回。我们可以看到，第1个匹配到的是这个字符串本身，从第2个开始，才是我们想要的字符串。
+
+	// 测试模式是否匹配字符串，括号里面的意思是
+	// 至少有一个a－z之间的字符存在
+	//match, _ := regexp.MatchString("p([a-z]+)ch", "peach")
+	//	fmt.Println(match)
+	// 上面我们直接使用了字符串匹配的正则表达式，
+	// 但是对于其他的正则匹配任务，你需要使用
+	// `Compile`来使用一个优化过的正则对象
+	//compile编译
+	r, _ := regexp.Compile("p([a-z]+)ch")
+	/*
+	*Regexp
+	 func Compile(expr string) (*Regexp, error) {
+		return compile(expr, syntax.Perl, false)
+	}
+	*/
+	//r是正则结构体对象
+	//fmt.Println(r) //p([a-z]+)ch
+	//fmt.Printf("%T \n",r) //*regexp.Regexp  这样子拿到的确实是值而不是地址
+	// 正则结构体对象有很多方法可以使用，比如上面的例子
+	// 也可以像下面这么写
+	//fmt.Println(r.MatchString("peach"))
+
+    //fmt.Println(r.FindString("11peach11"))
+	// 这个方法返回全局匹配的字符串和局部匹配的字符，比如
+	// 这里会返回匹配`p([a-z]+)ch`的字符串
+	// 和匹配`([a-z]+)`的字符串
+	//局部匹配的就是以这种[]形式出现的局部单元
+	//以数组形式返回
+	fmt.Println(r.FindStringSubmatch("peawwwwch  pea11111wwwwch hello world"))
+	fmt.Println(r.FindAllStringSubmatch("peawwwwch  pewerwerwerwerdfdsfwfswwch hello world",2))
+	//来循环这个数组
+	//params :=  r.FindStringSubmatch("peawwwwch hello world")
+	//循环的固定形式吗？第一个是index，第二个是value
+	//for _,match:= range params{
+	//	fmt.Println(match)
+	//
+	//}
+	/*
+	func (re *Regexp) FindAllStringSubmatch(s string, n int) [][]string {
+		if n < 0 {
+			n = len(s) + 1
+		}
+		var result [][]string
+		re.allMatches(s, nil, n, func(match []int) {
+			if result == nil {
+				result = make([][]string, 0, startSize)
+			}
+			slice := make([]string, len(match)/2)
+			for j := range slice {
+				if match[2*j] >= 0 {
+					slice[j] = s[match[2*j]:match[2*j+1]]
+				}
+			}
+			result = append(result, slice)
+		})
+		return result
+	}
+	*/
+	//n int参数 限制匹配数量
+	//	// 这个方法返回所有正则匹配的字符，不仅仅是第一个
+	//fmt.Println(r.FindAllStringSubmatch("peaddddchqwqweqwe hello world",-1))
+	//fmt.Println(r.FindStringIndex("peaddddcpeaddddchqwqweqwecpeaddddc hello world peaddddc"))
+	// 这个方法返回所有全局匹配和局部匹配的字符串起始索引
+	// 和结束索引
+	//[0 5 1 3] [6 11 7 9] [12 17 13 15]] 返回的是全部的索引以及局部的索引
+	//go 正则结束的位置就是你匹配到了以后继续找 发现没有匹配到就结束，这个搜索的位置就是结束位置
+	//fmt.Println(r.FindAllStringSubmatchIndex("peach punch punch", 4))
+	// 为这个方法提供一个正整数参数来限制匹配数量
+
+	//fmt.Println(r.FindAllString("peach punch pinch", -1))
+
+	// 这个方法查找第一次匹配的索引，并返回匹配字符串
+	// 的起始索引和结束索引，而不是匹配的字符串
+	//fmt.Println(r.FindStringIndex("peach punch"))
+
+	/*
+
+	[]byte byte数组
+	Unicode字符rune类型是和int32等价的类型，通常用于表示一个Unicode码点。
+	这两个名称可以互换使用。同样byte也是uint8类型的等价类型，byte类型一般用于强调数值是一个原始的数据而不是一个小的整数。
+	*/
+
+
+
+
+
+}
